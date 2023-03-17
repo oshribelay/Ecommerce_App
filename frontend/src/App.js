30664108ca67ee44e3c46dca26ae06eb55b5c629
oshribelay <oshri.belay@gmail.com>
2023-03-18 01:02:16 +0200

    added a navbar and app template

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import ResponsiveAppBar from './components/ResponsiveAppBar';
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <ResponsiveAppBar />
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
